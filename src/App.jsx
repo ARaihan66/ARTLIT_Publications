17c31192f74a1e24536d873f799239e4ccc50ed2
Abu Raihan <aburaihancse066@gmail.com>
2024-05-31 20:23:28 +0600

    Initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>ARTLIT PUBLICATION</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+        soluta aliquid unde! Quod cum eligendi ipsam quidem aperiam blanditiis
+        ex corrupti illo recusandae, dicta minus sint non delectus harum
+        dolorem.
+      </p>
+    </div>
+  );
+};
+
+export default App;
