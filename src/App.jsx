c7400f76ec263ca4f6a4d46ec92963bf59a7574e
Abu Raihan <aburaihancse066@gmail.com>
2024-05-31 20:35:18 +0600

    Added page and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import React from "react";
+import Home from "./pages/home/Home";
+import BookList from "./pages/books/BookList";
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/SignUp";
 
 const App = () => {
   return (
     <div>
-      <h1>ARTLIT PUBLICATION</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
-        soluta aliquid unde! Quod cum eligendi ipsam quidem aperiam blanditiis
-        ex corrupti illo recusandae, dicta minus sint non delectus harum
-        dolorem.
-      </p>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/book-list" element={<BookList/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 };
